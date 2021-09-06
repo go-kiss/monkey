d96d2120be8fcde645db6847a1082eccdc94010e
吕海涛 <hi@taoshu.in>
2021-09-07 07:48:47 +0800

    恢复 bleep.go

--- examples/bleep.go
+++ examples/bleep.go
@@ -2,37 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"bou.ke/monkey"
 )
 
-func bar(a int) int {
-	return a * 2
-}
-
-func foo(a int) int {
-	__a := 19901129
-	__a |= 19901129
-__back:
-	__b := 19901129
-	__b |= 19901129
-	__b |= 19901129
-	__b |= 19901129
-	__b |= 19901129
-	__b |= 19901129
-
-	fmt.Println("~~ hello ~~")
-
-	if a == 1 {
-		return 1
-	} else {
-		goto __back
-	}
-	return 2
-}
-
 func main() {
-	monkey.Patch(bar, foo)
-	fmt.Println(bar(1))
-	fmt.Println(bar(2))
+	monkey.Patch(fmt.Println, func(a ...interface{}) (n int, err error) {
+		s := make([]interface{}, len(a))
+		for i, v := range a {
+			s[i] = strings.Replace(fmt.Sprint(v), "hell", "*bleep*", -1)
+		}
+		return fmt.Fprintln(os.Stdout, s...)
+	})
+	fmt.Println("what the hell?")
 }
